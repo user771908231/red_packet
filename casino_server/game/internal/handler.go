34ef0a54db13158cb09a26735ddf4b1fe9291f3e
saplmm <1046439010@qq.com>
2016-08-30 00:47:00 +0800

    增加测试数据

--- casino_server/game/internal/handler.go
+++ casino_server/game/internal/handler.go
@@ -366,12 +366,14 @@ func handlerGame_TounamentBlind(args []interface{}) {
 	*d1.Ante = "10"
 	*d1.SmallBlind = "10"
 	*d1.RaiseTime = "150"
+	*d1.BlindLevel =  "1"
 	m.Data = append(m.Data, d1)
 
 	d2 := bbproto.NewGame_TounamentBlindBean()
 	*d2.Ante = "20"
 	*d2.SmallBlind = "20"
 	*d2.RaiseTime = "150"
+	*d2.BlindLevel =  "2"
 
 	m.Data = append(m.Data, d2)
 
@@ -379,6 +381,7 @@ func handlerGame_TounamentBlind(args []interface{}) {
 	*d3.Ante = "75"
 	*d3.SmallBlind = "150"
 	*d3.RaiseTime = "150"
+	*d3.BlindLevel =  "3"
 
 	m.Data = append(m.Data, d3)
 
