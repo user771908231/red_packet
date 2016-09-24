cd131efe62d78bfea7439499fe08e0195319c43c
saplmm <1046439010@qq.com>
2016-09-24 12:15:13 +0800

    麻将的... proto  可以编译通过

--- casino_majiang/msg/msg.go
+++ casino_majiang/msg/msg.go
@@ -2,14 +2,16 @@ package msg
 
 import (
 	"github.com/name5566/leaf/network/protobuf"
-	"casino_server/msg/bbprotogo"
+	casinoProto "casino_server/msg/bbprotogo"
+	//majiangProto "casino_majiang/msg/bbprotogo"
+
+
 )
 
 var Processor = protobuf.NewProcessor()
 
 func init() {
-	Processor.Register(&bbproto.NullMsg{})                          //0
-	Processor.Register(&bbproto.REQQuickConn{})                        //1
+	Processor.Register(&casinoProto.NullMsg{})                          //0
 }
 
 
