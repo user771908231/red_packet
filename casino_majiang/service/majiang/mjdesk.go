1a702d17d1188f1958e5be590a7a70a8cc24c87d
saplmm <1046439010@qq.com>
2016-09-28 21:17:31 +0800

    初始化桌子

--- casino_majiang/service/majiang/mjdesk.go
+++ casino_majiang/service/majiang/mjdesk.go
@@ -84,6 +84,19 @@ func NewMjRoom() *MjRoom {
 //返回一个麻将
 func NewMjDesk() *MjDesk {
 	ret := &MjDesk{}
+	ret.DeskId = new(int32)
+	ret.RoomId = new(int32)
+	ret.Status = new(int32)
 	ret.Password = new(string)
+	ret.Owner = new(uint32)
+	ret.CreateFee = new(int64)
+	ret.MjRoomType = new(int32)
+	ret.BoardsCout = new(int32)
+	ret.CapMax = new(int64)
+	ret.CardsNum = new(int32)
+	ret.Settlement = new(int32)
+	ret.BaseValue = new(int64)
+	ret.ZiMoRadio = new(int32)
+	ret.DianGangHuaRadio = new(int32)
 	return ret
 }
