1e433e2170b9ea09836f3e48d886e95516b748ab
saplmm <1046439010@qq.com>
2016-11-25 15:17:22 +0800

    斗地主 系统初始化代码精简

--- casino_doudizhu/main.go
+++ casino_doudizhu/main.go
@@ -2,37 +2,35 @@ package main
 
 import (
 	"github.com/name5566/leaf"
-	lconf "github.com/name5566/leaf/conf"
-	"os"
-	"casino_doudizhu/conf/config"
-	"casino_doudizhu/conf"
 	"casino_doudizhu/service/webserver"
 	"casino_doudizhu/game"
 	"casino_doudizhu/gate"
 	"casino_doudizhu/login"
 	"time"
+	"casino_common/common/sys"
+	"casino_doudizhu/conf"
+	"casino_doudizhu/conf/config"
 )
 
 func init() {
 	//初始化系统
-	e := config.InitConfig()
-	if e.IsError() {
-		//log.Error("config init failed.", e)
-		os.Exit(-1)
-	}
-
+	sys.SysInit(conf.Server.RedisAddr,
+		"dodizhu",
+		conf.Server.MongoIp,
+		conf.Server.MongoPort,
+		config.MJ_DBNAM,
+		config.DB_ENSURECOUNTER_KEY,
+		[]string{config.DBT_DDZ_DESK, config.DBT_T_TH_GAMENUMBER_SEQ})
 	time.Sleep(time.Second * 3)        //初始化3秒之后启动程序
 }
 
 func main() {
-	lconf.LogLevel = conf.Server.LogLevel
-	lconf.LogPath = conf.Server.LogPath
-
 	//后台管理
 	go func() {
 		webserver.InitCms()
 	}()
 
+	//启动leaf
 	leaf.Run(
 		game.Module,
 		gate.Module,
