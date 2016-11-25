f3ed1eded308134ea98f26f0edb546386b155083
saplmm <1046439010@qq.com>
2016-11-25 15:06:42 +0800

    通用的配置文件增加到公共库

---
+++ casino_common/common/sys/sys.go
@@ -0,0 +1,34 @@
+package sys
+
+import (
+	"fmt"
+	"time"
+	"casino_common/utils/redis"
+	"math/rand"
+	"runtime"
+	"casino_common/common/log"
+	"casino_common/common/db"
+)
+
+func SysInit(redisAddr string, logName string, mongoIp string, mongoPort int, mongoName string, mongoSeqKey string, mongoSeqTables []string) error {
+	InitRedis(redisAddr)
+	initRandSeed()
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	log.InitLogger(logName)        //初始化日志处理
+	db.InitMongoDb(mongoIp, mongoPort, mongoName, mongoSeqKey, mongoSeqTables)
+	return nil
+}
+
+//
+func InitRedis(addr string) error {
+	fmt.Println("3，初始化redis...")
+	data.InitRedis(addr)
+	return nil
+}
+
+func initRandSeed() {
+	fmt.Println("5，初始化随机数种子")
+	s := time.Now().UTC().UnixNano()
+	rand.Seed(s)
+}
+
