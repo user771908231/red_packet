db24c1363a0fc5bf0b86b313e1c287863663d731
saplmm <1046439010@qq.com>
2016-12-01 15:52:14 +0800

    no message

--- casino_login/gate/external.go
+++
@@ -1,7 +0,0 @@
-package gate
-
-import "casino_login/gate/internal"
-
-var (
-	Module = new(internal.Module)
-)
--- casino_login/login/external.go
+++
@@ -1,8 +0,0 @@
-package login
-
-import "casino_login/login/internal"
-
-var (
-	Module = new(internal.Module)
-	ChanRPC = internal.ChanRPC
-)
