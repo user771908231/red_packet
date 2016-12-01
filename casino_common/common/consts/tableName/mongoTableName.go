e85933c3b656b215130a198d2aa4dd53825912a7
saplmm <1046439010@qq.com>
2016-12-01 15:54:13 +0800

    no message

--- casino_common/common/consts/tableName/mongoTableName.go
+++
@@ -1,7 +0,0 @@
-package tableName
-
-const (
-	DBT_T_USER string = "t_user"
-	DBT_T_USER_DIAMOND_DETAILS string = "t_user_diamond_details"
-	DBT_T_TH_NOTICE = "t_th_notice"
-)
--- casino_common/common/model/baseModel.go
+++
@@ -1,7 +0,0 @@
-package model
-
-import "gopkg.in/mgo.v2/bson"
-
-type BaseMode interface {
-	GetMid() bson.ObjectId
-}
