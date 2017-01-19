596602faeefeec9f3523ccf79c8898dd2673bc07
saplmm <1046439010@qq.com>
2017-01-19 16:32:16 +0800

    agent 不用打日志

--- github.com/name5566/leaf/gate/gate.go
+++ github.com/name5566/leaf/gate/gate.go
@@ -137,10 +137,10 @@ func (a *agent) OnClose() {
 }
 
 func (a *agent) WriteMsg(msg interface{}) {
-	typeString := reflect.TypeOf(msg).String()
-	if !strings.Contains(typeString, "Heartbeat") {
-		clog.T("agent发送的信息 type[%v],\t\t content[%v]", typeString, msg)
-	}
+	//typeString := reflect.TypeOf(msg).String()
+	//if !strings.Contains(typeString, "Heartbeat") {
+	//	clog.T("agent发送的信息 type[%v],\t\t content[%v]", typeString, msg)
+	//}
 
 	if a.gate.Processor != nil {
 		data, err := a.gate.Processor.Marshal(msg)
