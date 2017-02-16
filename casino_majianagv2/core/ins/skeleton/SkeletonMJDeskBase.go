4145a325ff518364be38fe0746528a3fab117cf2
沈厚桦 <495430312@qq.com>
2017-02-16 18:10:59 +0800

    todo 实现

--- casino_majianagv2/core/ins/skeleton/SkeletonMJDeskBase.go
+++ casino_majianagv2/core/ins/skeleton/SkeletonMJDeskBase.go
@@ -8,6 +8,7 @@ import (
 	"casino_majiang/msg/protogo"
 	"casino_majiang/service/majiang"
 	"casino_majiang/msg/funcsInit"
+	"fmt"
 )
 
 //常见的get set 方法 需要放置在这里
@@ -21,32 +22,53 @@ func (r *SkeletonMJDesk) GetStatus() *data.MjDeskStatus {
 	return r.status
 }
 
-//todo 日志信息
+//日志信息
 func (r *SkeletonMJDesk) DlogDes() string {
-	return "todo"
+	s := fmt.Sprintf("[desk-%v-r-%v]", r.GetMJConfig().DeskId, r.GetMJConfig().CurrPlayCount)
+	return s
 }
 
-//todo 通过userId 找到对应的User
+//通过userId 找到对应的User
 func (r *SkeletonMJDesk) GetUserByUserId(userId uint32) api.MjUser {
+	for _, u := range r.GetUsers() {
+		if u != nil && u.GetUserId() == userId {
+			return u
+		}
+	}
+
 	return nil
 }
 
-//todo 广播
+//广播
 func (d *SkeletonMJDesk) BroadCastProto(p proto.Message) {
-
+	for _, u := range d.Users {
+		if u != nil {
+			u.WriteMsg(p)
+		}
+	}
 }
 
-//todo 是否有牌可以摸
+//是否有牌可以摸
 func (d *SkeletonMJDesk) HandPaiCanMo() bool {
-	return false
+	if d.GetRemainPaiCount() == 0 {
+		return false
+	} else {
+		return true
+	}
 }
 func (d *SkeletonMJDesk) GetCheckCase() *data.CheckCase {
 	return d.CheckCase
 }
 
-// todo 游戏中玩家的人数
+//游戏中玩家的人数
 func (d *SkeletonMJDesk) GetGamingCount() int32 {
-	return 0
+	var gamingCount int32 = 0 //正在游戏中的玩家数量
+	for _, user := range d.GetUsers() {
+		if user != nil && user.GetStatus().IsGaming() {
+			gamingCount ++
+		}
+	}
+	return gamingCount
 }
 
 func (d *SkeletonMJDesk) GetUsers() []api.MjUser {
