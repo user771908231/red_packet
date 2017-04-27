22e26585e366e1d939cd9fd9254f63816e6b8935
james wang <yiming.520@live.com>
2017-04-27 15:18:33 +0000

    注释微信配置，合并common改动

--- model/weixinModel/init.go
+++ model/weixinModel/init.go
@@ -2,10 +2,10 @@ package weixinModel
 
 const (
 	//公共
-	WX_APP_ID string = "wx804b74c45a08e6e1"
-	WX_MCH_ID string = "1394403202"
+	WX_APP_ID string = "wx804b74c45a08e6e1"  //old:wx804b74c45a08e6e1
+	WX_MCH_ID string = "1394403202"  //old:1394403202
 	//用于公众号支付 mch
-	WX_API_KEY string = "DD18Secoh98GdO5Ikl1Kehj4IqxMP998"
+	WX_API_KEY string = "DD18Secoh98GdO5Ikl1Kehj4IqxMP998"  //old:DD18Secoh98GdO5Ikl1Kehj4IqxMP998
 	//用于Oauth2.0登录，收发消息等 mp
-	WX_APP_SECRET string = "4b81d61d2f2773c7752396ee2e8ca96e"
+	WX_APP_SECRET string = "4b81d61d2f2773c7752396ee2e8ca96e"   //old:4b81d61d2f2773c7752396ee2e8ca96e
 )
