2fa089c700285b8aec46a7c04b95b8211ca3ef34
user771908231 <771908231@qq.com>
2018-03-23 18:38:59 +0800

    订单自增ID

--- casino_redpack/conf/config/superDbCons.go
+++ casino_redpack/conf/config/superDbCons.go
@@ -4,4 +4,5 @@ const (
 	SUPER_DBNAM             = "test"
 	DB_USER_SEQ             = "super_user_id"       //user_id自增键
 	DBT_SUPER_LOGS          = "t_log"               //桌子的信息
+	ORDER_KEY_ID			= "t_order_key_id"		//订单的自增ID
 )
