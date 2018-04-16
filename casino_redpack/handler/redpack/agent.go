e6e3312b5eb061d7ac54e938774cc160d408db55
user771908231 <771908231@qq.com>
2018-04-16 17:33:14 +0800

    二维码

--- casino_redpack/handler/redpack/agent.go
+++ casino_redpack/handler/redpack/agent.go
@@ -5,16 +5,19 @@ import (
 
 	"github.com/skip2/go-qrcode"
 	"fmt"
+	"image/color"
 )
 
 //代理中心
 func AgentHandler(ctx *modules.Context) {
-	//user_info := ctx.IsLogin()
-	//str := fmt.Sprintf("%s/home/sign_up?extension_id=%s",ctx.Req.Host,ctx.IsLogin().Id)
+	//二维码的存储路径
 	FilePath := fmt.Sprintf("public/qrcode/%dqrcodr.png",ctx.IsLogin().Id)
-	UserQrcode := fmt.Sprintf("qrcode/%dqrcodr.png",ctx.IsLogin().Id)
+	//二维码所在路径
+	UserQrcode := fmt.Sprintf("/qrcode/%dqrcodr.png",ctx.IsLogin().Id)
+	//二维码中的URL信息
+	URl := fmt.Sprintf("%s/home/sign_up?extension_id=%d",ctx.Req.Host,ctx.IsLogin().Id)
 	fmt.Println(FilePath)
-	err := qrcode.WriteFile("https://www.baidu.com", qrcode.Medium, 256, FilePath)
+	err := qrcode.WriteColorFile(URl, qrcode.Medium, 200,color.Transparent,color.Black,FilePath)
 
 	if err !=nil {
 		UserQrcode = ""
